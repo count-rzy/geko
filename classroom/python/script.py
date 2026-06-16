6f1e82c3e5dc5b52e5f47c74bab19692e9e2214c
HP <suren12.128@gmail.com>
2026-06-16 22:35:47 +0400

    .

--- classroom/python/script.py
+++ classroom/python/script.py
@@ -1,17 +1,29 @@
-import math
-def calculator():
-  while True:
-    print("Bari galust kalkulator!")
-    print("quit anelu hamar greq 'q'.")
-    print("greq dzer gorcuxutyuny/xndiry/varjutyuny: ")
-    expression = input()
-    if expression == "q":
-      break
-    try:
-      result =  eval(expression)
-      print("Patasxan :", result)
-    except SyntaxError:
-      print("sxal ka.")
-    except ZeroDivisionError:
-      print("Zroin bajanely argelvac e.")
-calculator()
+import json
+import random
+
+
+
+results = {}
+
+for i in range(100):
+    zar1 = random.randint(1, 6)
+    zar2 = random.randint(1, 6)
+
+    if zar1 > zar2:
+        combo = str(zar2) + '//' + str(zar1)
+    else:
+        combo = str(zar1) + '//' + str(zar2)
+
+    if combo in results:
+        results[combo] = results[combo] + 1
+    else:
+        results[combo] = 1
+
+with open('dice_results.json', 'w') as file:
+    json.dump(results, file, indent=9)
+
+
+
+
+
+print('patrsat e, stugeq json fayly')

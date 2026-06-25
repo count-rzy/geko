f3b0e5eeada201edca21b725deb930fc1b46cb95
HP <suren12.128@gmail.com>
2026-06-25 15:59:09 +0400

    .

---
+++ classroom/python/regular/script.py
@@ -0,0 +1,9 @@
+a = int(input('greq koxmi erkarutyuny(a>=1)   '))
+n = int(input('greq koxmi qanaky(n>=1)        '))
+
+
+if a < 1 or n < 1: 
+    print('bacasakan tiv ogtagorcel argelvum e')
+else:
+    p = 4 * int(a) * int(n)
+    print(p, '  aha paragicy')
